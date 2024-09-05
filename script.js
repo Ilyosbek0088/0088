1274f61c4e92fbac0e0113b84fd9f6b01b1413d8
Ilyosbek0088 <ilyosbek0088@gmail.com>
2024-09-05 17:08:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let brand = prompt("Your Hobby")
+let model = prompt("Age")
+let color = prompt("Best Freind")
+let price = prompt("Job")
+let speed = prompt("What you hate most?")
+let data = " Your Hobby " + brand + " Age " + model + " Best Freind " + color + " Job " + price + " What you hate most? " + speed;
+console.log(data)
