ebb6200c8fb8d14d9dfea23ee6cdc155f8115cf0
Ilyosbek0088 <ilyosbek0088@gmail.com>
2024-09-15 15:06:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-let brand = prompt("Your Hobby")
-let model = prompt("Age")
-let color = prompt("Best Freind")
-let price = prompt("Job")
-let speed = prompt("What you hate most?")
-let data = " Your Hobby " + brand + " Age " + model + " Best Freind " + color + " Job " + price + " What you hate most? " + speed;
-console.log(data)
+
